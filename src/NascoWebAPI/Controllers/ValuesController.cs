f21ff65b01cf6ce12f995dee3147c1b59dc277af
DESKTOP-1HBCBDV\Thai Phan <thaipg@dsc.vn>
2020-04-04 11:05:59 +0700

    commit code call VNPT test

--- src/NascoWebAPI/Controllers/ValuesController.cs
+++ src/NascoWebAPI/Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using VNPTPublishService;
+using Microsoft.AspNetCore.Server.Kestrel;
 
 namespace NascoWebAPI.Controllers
 {
@@ -27,8 +29,63 @@ namespace NascoWebAPI.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
-        {
+        public Task<ImportAndPublishInvResponse> Post()
+        { 
+            ImportAndPublishInvRequestBody requestBody = new ImportAndPublishInvRequestBody();
+            requestBody.Account = "nascoservice";
+            requestBody.ACpass = "123456aA@";
+            requestBody.xmlInvData = @"<xmlInvData>
+      	    <Invoices>
+			    <Inv>
+			    <Invoice>
+			    <CusCode>12356</CusCode>
+			    <CusName>Thai Phan</CusName>
+			    <Buyer>test</Buyer>
+			    <CusAddress>355 tung thien vuong</CusAddress>
+			    <CusPhone>000000000</CusPhone>
+			    <CusTaxCode>12345678</CusTaxCode>
+			    <PaymentMethod>Phương thức thanh toán</PaymentMethod>
+			    <KindOfService>Tháng hóa đơn</KindOfService>
+			    <Products>
+			    <Product>
+			    <ProdName>TEST</ProdName>
+			    <ProdUnit>Đơn vị tính</ProdUnit>
+			    <ProdQuantity>1</ProdQuantity>
+			    <ProdPrice>123</ProdPrice>
+			    <Amount>123</Amount>
+			    </Product>
+			    </Products>
+			    <Total>0</Total>
+			    <DiscountAmount>0</DiscountAmount>
+			    <VATRate>10</VATRate>
+			    <VATAmount>1000</VATAmount>
+			    <Amount>0</Amount>
+			    <AmountInWords>vai</AmountInWords>
+			    <Extra>Các nội dung mở rộng</Extra>
+			    <ArisingDate>04/03/2020</ArisingDate>
+			    <PaymentStatus>1</PaymentStatus>
+			    </Invoice>
+			    </Inv>
+		    </Invoices>
+          </xmlInvData>";
+            requestBody.username = "";
+            requestBody.password = "";
+            requestBody.pattern = "01GTKT0/001";
+            requestBody.serial = "NC/18E";
+            requestBody.convert = 0;
+            PublishServiceSoapClient.EndpointConfiguration endpoint = new PublishServiceSoapClient.EndpointConfiguration();
+            PublishServiceSoapClient client = new PublishServiceSoapClient(endpoint);
+            var result = client.ImportAndPublishInvAsync(
+                requestBody.Account,
+                requestBody.ACpass,
+                requestBody.xmlInvData,
+                requestBody.username,
+                requestBody.password,
+                requestBody.pattern,
+                requestBody.serial,
+                requestBody.convert
+                );
+            return result;
         }
 
         // PUT api/values/5
