aa3291a425ca5eb5dbc431f1b6d6b50fa46e667e
Dung <ngngocdung94@gmail.com>
2020-04-07 17:35:43 +0700

    fix api vnpt

--- src/NascoWebAPI/Controllers/ValuesController.cs
+++ src/NascoWebAPI/Controllers/ValuesController.cs
@@ -30,50 +30,50 @@ namespace NascoWebAPI.Controllers
 
         // POST api/values
         [HttpPost]
-        public async Task<ImportAndPublishInvResponse> Post()
+        public async Task<ImportAndPublishInvResponse> Post([FromBody]ImportAndPublishInvRequestBody requestBody)
         {
-            ImportAndPublishInvRequestBody requestBody = new ImportAndPublishInvRequestBody();
-            requestBody.Account = "nascoadmin";
-            requestBody.ACpass = "123456aA@";
-            requestBody.xmlInvData = @"<Invoices>
-			    <Inv>
-                <key>dungtest1</key> 
-			    <Invoice>
-			    <CusCode>12356</CusCode>
-			    <CusName>Thai Phan</CusName>
-			    <Buyer>test</Buyer>
-			    <CusAddress>355 tung thien vuong</CusAddress>
-			    <CusPhone>0946669698</CusPhone>
-			    <CusTaxCode>0108497732</CusTaxCode>
-			    <PaymentMethod>TM</PaymentMethod>
-			    <KindOfService>04</KindOfService>
-			    <Extra>Khuyen mai</Extra>
-			    <Products>
-			    <Product>
-			    <ProdName>TEST</ProdName>
-                <Code>RV</Code>
-			    <ProdUnit>cai</ProdUnit>
-			    <ProdQuantity>1</ProdQuantity>
-			    <ProdPrice>1000000</ProdPrice>
-			    <Amount>1000000</Amount>
-			    </Product>
-			    </Products>
-			    <Total>1000000</Total>
-			    <DiscountAmount>0</DiscountAmount>
-			    <VATRate>10</VATRate>
-			    <VATAmount>100000</VATAmount>
-			    <Amount>1100000</Amount>
-			    <AmountInWords>mot trieu mot tran ngan việt nam đồng</AmountInWords>
-                <ArisingDate>04/03/2020</ArisingDate>
-			    <PaymentStatus>1</PaymentStatus>
-			    </Invoice>
-			    </Inv>
-		    </Invoices>";
-            requestBody.username = "nascoservice";
-            requestBody.password = "123456aA@";
-            requestBody.pattern = "01GTKT0/001";
-            requestBody.serial = "NC/18E";
-            requestBody.convert = 1;
+            //      ImportAndPublishInvRequestBody requestBody = new ImportAndPublishInvRequestBody();
+            //      requestBody.Account = "nascoadmin";
+            //      requestBody.ACpass = "123456aA@";
+            //      requestBody.xmlInvData = @"<Invoices>
+            // <Inv>
+            //          <key>dungtest1</key> 
+            // <Invoice>
+            // <CusCode>12356</CusCode>
+            // <CusName>Thai Phan</CusName>
+            // <Buyer>test</Buyer>
+            // <CusAddress>355 tung thien vuong</CusAddress>
+            // <CusPhone>0946669698</CusPhone>
+            // <CusTaxCode>0108497732</CusTaxCode>
+            // <PaymentMethod>TM</PaymentMethod>
+            // <KindOfService>04</KindOfService>
+            // <Extra>Khuyen mai</Extra>
+            // <Products>
+            // <Product>
+            // <ProdName>TEST</ProdName>
+            //          <Code>RV</Code>
+            // <ProdUnit>cai</ProdUnit>
+            // <ProdQuantity>1</ProdQuantity>
+            // <ProdPrice>1000000</ProdPrice>
+            // <Amount>1000000</Amount>
+            // </Product>
+            // </Products>
+            // <Total>1000000</Total>
+            // <DiscountAmount>0</DiscountAmount>
+            // <VATRate>10</VATRate>
+            // <VATAmount>100000</VATAmount>
+            // <Amount>1100000</Amount>
+            // <AmountInWords>mot trieu mot tran ngan việt nam đồng</AmountInWords>
+            //          <ArisingDate>04/03/2020</ArisingDate>
+            // <PaymentStatus>1</PaymentStatus>
+            // </Invoice>
+            // </Inv>
+            //</Invoices>";
+            //      requestBody.username = "nascoservice";
+            //      requestBody.password = "123456aA@";
+            //      requestBody.pattern = "01GTKT0/001";
+            //      requestBody.serial = "NC/18E";
+            //      requestBody.convert = 1;
             PublishServiceSoapClient.EndpointConfiguration endpoint = new PublishServiceSoapClient.EndpointConfiguration();
             PublishServiceSoapClient client = new PublishServiceSoapClient(endpoint);
             var result = client.ImportAndPublishInvAsync(
